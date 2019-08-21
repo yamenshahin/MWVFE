026c214f52881d068694784528d5281cc78b7e3a
Yamen Shahin <yamenshahin@gmail.com>
2019-08-21 17:16:18 +0200

    add comp

--- pages/index.vue
+++ pages/index.vue
@@ -1,80 +1,9 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        MWV_Front_end
-      </h1>
-      <h2 class="subtitle">
-        Man With Van Front-end
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-    <div>
-      <h2>Example heading <b-badge>New</b-badge></h2>
-      <h3>Example heading <b-badge>New</b-badge></h3>
-      <h4>Example heading <b-badge>New</b-badge></h4>
-      <h5>Example heading <b-badge>New</b-badge></h5>
-      <h6>Example heading <b-badge>New</b-badge></h6>
-    </div>
-    <div class="text-center">
-      <b-button variant="primary">
-        Notifications <b-badge variant="light">4</b-badge>
-      </b-button>
-    </div>
+    <div></div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
-export default {
-  components: {
-    Logo
-  }
-}
+export default {}
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
